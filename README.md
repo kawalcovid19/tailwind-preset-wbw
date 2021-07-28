d3ba422ec74fd366b02a066b1a49c79f10968945
Resi Respati <resir014@gmail.com>
2021-07-28 17:41:36 +0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# `@kawalcovid19/tailwind-preset-wbw`
+
+Tailwind preset used for projects across the Kawal COVID-19 + WargaBantuWarga projects.
+
+## Installation
+
+```bash
+yarn add @kawalcovid19/tailwind-preset-wbw @tailwindcss/forms @tailwindcss/typography
+```
+
+## Usage
+
+To use this preset, add this preset into the `presets` key in your project's `tailwind.config.js`:
+
+```js
+module.exports = {
+  presets: [require('@kawalcovid19/tailwind-preset-wbw')],
+  theme: {
+    // ...
+  },
+};
+```
---
+++ src/tailwind-preset-wbw.js
@@ -0,0 +1,43 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig}*/
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // WargaBantuWarga.com brand colour
+        brand: {
+          DEFAULT: '#1667c2',
+          50: '#c5ddf8',
+          100: '#aed0f6',
+          200: '#80b5f1',
+          300: '#539beb',
+          400: '#2580e6',
+          500: '#1667c2',
+          600: '#114f94',
+          700: '#0c3666',
+          800: '#061e39',
+          900: '#01060b',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            hr: {
+              borderColor: '#9ca3af',
+              marginTop: '1.5rem',
+              marginBottom: '1.5rem',
+            },
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+};
